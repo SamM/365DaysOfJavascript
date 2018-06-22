131bc73658433c05ec80ee02c5bf1a0f683a300c
Sam Mulqueen <sam.mulqueen.nz@gmail.com>
2018-06-22 17:18:48 +1200

    Updated PortfolioPage

--- days/0/PortfolioPage.js
+++ days/0/PortfolioPage.js
@@ -14,9 +14,67 @@ define(function(require){
 
     var loader = LoadDays(start_day, end_day);
 
+    var loadingMsg = document.createElement('div');
+    loadingMsg.className = "loading";
+
+    loadingMsg.innerHTML = "Loading Days...";
+
+    document.body.appendChild(loadingMsg);
+
+    var days = document.createElement('div');
+    days.className = "days";
+
+    var daysHeading = document.createElement('h2');
+    daysHeading.innerHTML = "0 Days Completed";
+    
+    days.appendChild(daysHeading);
+    document.body.appendChild(days);
+
+    var loaded = 0;
+
+    function DayElement(info){
+        var day = document.createElement('div');
+        day.className = "day";
+
+        var dayHeading = document.createElement('h3');
+        dayHeading.innerHTML = "Day "+info.day;
+        day.appendChild(dayHeading);
+
+        var date = document.createElement('div');
+        date.className = "date";
+        date.innerHTML = '<strong>Date:</strong> '+info.date;
+        day.appendChild(date);
+
+        var scripts = document.createElement('div');
+        scripts.className = "scripts";
+        scripts.innerHTML = "<h4>Scripts:</h4>";
+
+        var scriptList = document.createElement('ul');
+        scripts.appendChild(scriptList);
+
+        Object.keys(info.scripts).forEach(function(key){
+            var link = document.createElement('a');
+            link.href = "days/"+info.day+"/"+info.scripts[key].filename;
+            link.innerHTML = key;
+            link.target = "_blank";
+            var listItem = document.createElement('li');
+            listItem.appendChild(link);
+            scriptList.appendChild(listItem);
+        })
+
+        day.appendChild(scripts);
+
+
+        return day;
+    }
+
     loader.onLoad(function(index, info){
         console.log("Day "+index+": ");
         console.log(info);
+        loaded++;
+        daysHeading.innerHTML = loaded+(loaded==1?" Day Completed":" Days Completed");
+        var day = DayElement(info);
+        days.appendChild(day);
     });
 
     loader.onError(function(index, error){
